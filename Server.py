6710909f61ddaa659bf76fde2490c96d421a41c0
Seyed Mojtaba Esmaili <mo93esm@gmail.com>
2022-07-02 23:57:46 +0430

    add receive function

--- Server.py
+++ Server.py
@@ -18,5 +18,27 @@ clients = {}
 def broadcast(msg):
     for client in clients:
         client.send(msg)
-        
+
+def handle(client):
+    pass
+
+
+def receive():
+    while True :
+        client,addr = server.accept()
+        print(f"[{addr}] Connected")
+
+        client.send("username".encode(FORMAT))
+        username = client.recv(100).decode(FORMAT)
+        clients[client] = username
+
+        broadcast(f"{username} joined the chat!".encode(FORMAT))
+        client.send("connected to the chat".encode(FORMAT))
+
+        try:
+            thread = threading.Thread(target=handle,args=(client,))
+            thread.start()
+        except:
+            print("somthing went wrong!!!")
+
 print(f"Server is up with [{SERVER}] ...")
