6c1078b35c774fe6818a2557f8319097bb379d8b
Seyed Mojtaba Esmaili <mo93esm@gmail.com>
2022-07-07 02:59:46 +0430

    client is conecting

--- Client.py
+++ Client.py
@@ -6,3 +6,8 @@ SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER,PORT)
 FORMAT = "utf-8"
 DISCONNET_MSG = "leave"
+
+username = input("enter your username : ")
+
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(ADDR)
