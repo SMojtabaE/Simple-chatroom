b386e94e79b30c1af4daa01279e9a0de94be5797
Seyed Mojtaba Esmaili <mo93esm@gmail.com>
2022-07-08 22:02:29 +0430

    add receive function

--- Client.py
+++ Client.py
@@ -12,6 +12,21 @@ username = input("enter your username : ")
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect(ADDR)
 
+
+def receive():
+    while True:
+        try:
+            massage = client.recv(1024).decode(FORMAT)
+            if massage == "username":
+                client.send(username.encode(FORMAT))
+            else:
+                print(massage)
+        except:
+            print("there is somthing Wrong!")
+            client.close()
+            break
+
+        
 def write():
     while True:
         massage = input("('leave'->exit):")
